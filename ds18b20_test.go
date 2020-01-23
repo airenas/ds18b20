6c34f4ac7c9c934923e1ad7df4b26cb9bbe6a341
airenas <airenass@gmail.com>
2020-01-23 19:40:59 +0200

    Comment tests - as they are integration tests

--- ds18b20_test.go
+++ ds18b20_test.go
@@ -1,23 +1,19 @@
 package ds18b20
 
-import (
-	"testing"
-)
+// func TestSensors(t *testing.T) {
+// 	sensors, err := Sensors()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-func TestSensors(t *testing.T) {
-	sensors, err := Sensors()
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	t.Logf("sensors: %v", sensors)
+// }
 
-	t.Logf("sensors: %v", sensors)
-}
+// func TestTemperature(t *testing.T) {
+// 	temperature, err := Temperature("28-0000010ae26f")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-func TestTemperature(t *testing.T) {
-	temperature, err := Temperature("28-0000010ae26f")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Logf("temperature: %.4f°C", temperature)
-}
+// 	t.Logf("temperature: %.4f°C", temperature)
+// }
